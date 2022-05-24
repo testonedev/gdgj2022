16f78aaa60d9bee8ef7d668a8663279512300ed4
MrTestOne <mto@mt-one.net>
2022-05-24 21:19:09 +0800

    implement spawner for souls

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,25 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UltEvents;
+using Lean.Pool;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private Soul soulPrefab;
-    [SerializeField] private Transform spawnLocation;
-    [SerializeField] private UltEvent<Soul> onSoulSpawnedEvent;
+    public GameObject soulPrefab;
+    public Transform spawnLocation;
+    public float spawnRate;
+
+    public UltEvent<GameObject> onSoulSpawnedEvent;
+
+    //runtime
+    private float nextSpawnTime;
+    private GameObject spawnedObject;
 
     void Start()
     {
-        
+        nextSpawnTime = Time.time + (1 / spawnRate);
     }
 
     void Update()
     {
+        if (!isActiveAndEnabled) return;
         
+        if (nextSpawnTime <= Time.time) SpawnSoul();
     }
 
     public void SpawnSoul()
     {
-        throw new System.NotImplementedException();
+        nextSpawnTime = Time.time + (1 / spawnRate);
+
+        spawnedObject = LeanPool.Spawn(soulPrefab);
+        spawnedObject.transform.position = spawnLocation.position;
+        spawnedObject.GetComponent<Soul>().StartObject();
+
+        onSoulSpawnedEvent.Invoke(spawnedObject);
     }
 }
