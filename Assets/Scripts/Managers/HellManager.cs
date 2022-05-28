5a2c921b98f25e50bbaf15ffc31e3ebfb3c956cf
MrTestOne <mto@mt-one.net>
2022-05-28 13:03:10 +0800

    Basic random AI

--- Assets/Scripts/Managers/HellManager.cs
+++ Assets/Scripts/Managers/HellManager.cs
@@ -5,4 +5,30 @@ using UnityEngine;
 public class HellManager : ParkManager
 {
     public override Alignment Alignment { get => Alignment.Hell; }
+    public float upgradeInterval = 3f;
+
+    //runtime
+    private float nextTime;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        nextTime = Time.time + upgradeInterval;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (nextTime <= Time.time) TryUpgrading();
+    }
+
+    private void TryUpgrading()
+    {
+        //tries randomly to upgrade a plot
+        plots[Random.Range(0, 6), 0].UpgradePlot();
+
+        nextTime = Time.time + upgradeInterval;
+    }
 }
